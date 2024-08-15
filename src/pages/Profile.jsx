739ac4a0cc73144295a782118f2ef93440774832
Saif <droconos99@gmail.com>
2024-08-16 00:11:05 +0600

    Bug Fix: Fixed a title error in profile page.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -39,40 +39,45 @@ function Profile() {
 
    const isAuthor = userDataFromStore?.$id === user?.$id;
 
-   return (
-      <ParentContainer>
-         <Container className="space-y-5 max-w-7xl">
-            <div className="bg-white dark:bg-black p-10 rounded-2xl">
-               <h1 className="text-2xl font-bold text-center mb-4">Profile</h1>
-               {userDataError && <ErrorMessage error={userDataError} />}
-               {userDataLoading && !userDataError ? (
-                  <Loader />
-               ) : (
-                  <div className="flex sm:flex-row flex-col space-x-7">
-                     <img
-                        src={user?.profilePicture || "/blank-dp.png"}
-                        alt="Profile Picture"
-                        className="w-56 h-56 rounded-full"
-                     />
-                     <div className="max-w-3xl xl:max-w-full mt-3 space-y-3">
-                        <h1 className="text-3xl font-semibold">{user?.name}</h1>
-                        <p className="text-lg font-medium text-gray-700 dark:text-gray-400">
-                           {user?.bio}
-                        </p>
-                        {isAuthor && (
-                           <Button
-                              text={"Edit profile"}
-                              bgNumber={1}
-                              className="w-28"
-                              onClick={() => navigate("/edit-profile")}
-                           />
-                        )}
-                     </div>
+   const ProfileHeader = () => {
+      return (
+         <div className="bg-white dark:bg-black p-10 rounded-2xl">
+            <h1 className="text-2xl font-bold text-center mb-4">Profile</h1>
+            {userDataError && <ErrorMessage error={userDataError} />}
+            {userDataLoading && !userDataError ? (
+               <Loader />
+            ) : (
+               <div className="flex sm:flex-row flex-col space-x-7">
+                  <img
+                     src={user?.profilePicture || "/blank-dp.png"}
+                     alt="Profile Picture"
+                     className="w-56 h-56 rounded-full"
+                  />
+                  <div className="max-w-3xl xl:max-w-full mt-3 space-y-3">
+                     <h1 className="text-3xl font-semibold">{user?.name}</h1>
+                     <p className="text-lg font-medium text-gray-700 dark:text-gray-400">
+                        {user?.bio}
+                     </p>
+                     {isAuthor && (
+                        <Button
+                           text={"Edit profile"}
+                           bgNumber={1}
+                           className="w-28"
+                           onClick={() => navigate("/edit-profile")}
+                        />
+                     )}
                   </div>
-               )}
-            </div>
+               </div>
+            )}
+         </div>
+      );
+   };
+
+   const UserPosts = () => {
+      return (
+         <>
             <h1 className="text-2xl font-bold text-center">
-               {isAuthor ? "My" : (user === undefined ? "User" : user?.name) + "'s"} posts
+               {isAuthor ? "My" : `${user?.name ?? "User"}'s`} posts
             </h1>
             {allPosts?.length >= 0 ? (
                <LoadCards posts={allPosts} />
@@ -86,6 +91,15 @@ function Profile() {
                   No more posts. {isAuthor && "Add more posts!"}
                </p>
             )}
+         </>
+      );
+   };
+
+   return (
+      <ParentContainer>
+         <Container className="space-y-5 max-w-7xl">
+            <ProfileHeader />
+            <UserPosts />
          </Container>
       </ParentContainer>
    );
